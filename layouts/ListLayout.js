81fcdcd1bd225de1ff5d9e5ff24773f5414fb3f8
hanji <o-range@kakao.com>
2022-01-23 21:19:49 +0900

    tag 주소 수정

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -50,7 +50,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary, tags, folder } = frontMatter
             return (
               <li key={slug} className="py-4">
                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
@@ -64,7 +64,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link
-                          href={`/${type}/${slug}`}
+                          href={`/${folder}/${slug}`}
                           className="text-gray-900 dark:text-gray-100"
                         >
                           {title}
