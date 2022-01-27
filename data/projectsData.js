d572e3073da66e4834a6a8f7d2946d59053d06a1
hanji <o-range@kakao.com>
2022-01-27 10:57:47 +0900

    project data 변경

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,15 @@
 const projectsData = [
   {
     title: 'Spring',
-    description: `spring`,
+    description: `API`,
     imgSrc: `/static/images/spring.svg`,
-    href: 'https://spring.io/',
+    href: 'https://github.com/abhidhamma-java/TIL/blob/main/sideproject/spring/selfhelp.md',
   },
   {
     title: 'React',
-    description: `react`,
+    description: `react로 만든 일기장`,
     imgSrc: `/static/images/react.svg`,
-    href: 'https://reactjs.org/',
+    href: 'https://github.com/abhidhamma-private/disorders-ui',
   },
 ]
 
